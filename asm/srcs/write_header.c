fd4c0ba71b92f860396963bbc6f389842c0010cb
David Nathanael <ddela-cr@student.42.fr>
2016-09-08 00:23:25 +0200

    Rewritten ft_write_unsigned_int in asm

--- asm/srcs/write_header.c
+++ asm/srcs/write_header.c
@@ -11,23 +11,20 @@
 /* ************************************************************************** */
 
 #include "asm.h"
-#include "limits.h"
 
 static void		ft_write_unsigned_int(int fd, unsigned int nb)
 {
-	int		oct_1;
-	int		oct_2;
-	int		oct_3;
-	int		oct_4;
+	int		offset;
+	int		value;
 
-	oct_1 = (nb >> 24) & 0xff;
-	oct_2 = (nb >> 16) & 0xff;
-	oct_3 = (nb >> 8) & 0xff;
-	oct_4 = nb & 0xff;
-	write(fd, &oct_1, 1);
-	write(fd, &oct_2, 1);
-	write(fd, &oct_3, 1);
-	write(fd, &oct_4, 1);
+	offset = 32;
+	value = 0;
+	while (offset)
+	{
+		offset -= 8;
+		value = (nb >> offset) & 0xff;
+		write(fd, &value, 1);
+	}
 }
 
 static void		ft_write_string(int fd, char *string, int size)
